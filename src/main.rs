c27fd4c1c0668702105ab599d0a5d354d5239694
sypeng <sypeng@iflytek.com>
2021-02-21 15:28:57 +0800

    cargo使用2018版本

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,14 @@
-extern crate minigrep;
-
+use minigrep::{run, Config};
 use std::env;
 use std::process;
 
-use minigrep::Config;
-
-
 fn main() {
-
     let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    if let Err(e) = minigrep::run(config) {
+    if let Err(e) = run(config) {
         eprintln!("Application error: {}", e);
         process::exit(1);
     }
